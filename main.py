836b78de0422f154c164cbc1126bb45a906b5823
rhhen122 <rokyhenderson222@gmail.com>
2025-01-28 15:47:35 +1300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ q1a = "n"
 
 q2 = "did anything happen in taimen sqaure in 1918?"
 q2a = "n"
+
 q3 = "does jinping look like whinie the pooh"
 q3a = "n"
 
@@ -19,16 +20,16 @@ q5a = "y"
 print(q1)
 q1ua = input("")
 if q1ua == q1a:
-    socialcredit += (10000)
+    socialcredit += (100)
 else:
-    socialcredit -= (99900)
+    socialcredit -= (99)
 
 print(q2)
 q2ua = input("")
 if q2ua == q2a:
-    socialcredit += (1000)
+    socialcredit += (100)
 else:
-    socialcredit -= (9999)
+    socialcredit -= (99)
 
 print(q3)
 q3ua = input("")
@@ -42,14 +43,14 @@ q4ua = input("")
 if q4ua == q4a:
     socialcredit += (0.5)
 else:
-    socialcredit -= (1000)
+    socialcredit -= (99)
 
 print(q5)
 q5ua = input("")
 if q5ua == q5a:
-    socialcredit += (9999.78901)
+    socialcredit += (9.78901)
 else:
-    socialcredit -= (80000)
+    socialcredit -= (1000)
 #prints social credit
 print("this is your social credit")
 print("i wonder if you like to suck xijingpiengs toes")
